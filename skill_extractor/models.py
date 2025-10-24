ea8ba8757debae0f547f1cdb4e6d6a44c185300b
Mukhammad Rizki <rizkyRE7@gmail.com>
2025-10-24 10:36:56 +0700

    Created Skill Extractor Model and Migration

--- skill_extractor/models.py
+++ skill_extractor/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from resource.models import Course, Skill
 
 # Create your models here.
+
+class ExtractedCourse(models.Model):
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    extracted_skills = models.TextField(null=True)
+
+class ExtractedSkill(models.Model):
+    skill_id = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    extracted_skills = models.TextField()
