c3d7591f466938dc2f98217344041156304065af
Mukhammad Rizki <rizkyRE7@gmail.com>
2025-10-24 10:27:41 +0700

    Created Resouce Model and Migration

--- resource/models.py
+++ resource/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    learning_outcome = models.TextField()
+    content = models.TextField(null=True)
+
+class SkillFramework(models.Model):
+    name = models.CharField(max_length=255)
+
+class Skill(models.Model):
+    skill_framework_id = models.ForeignKey(SkillFramework, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+
